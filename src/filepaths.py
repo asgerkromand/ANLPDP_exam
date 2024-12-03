1c0242016b27b4f5d1d9776b0dd471b4980f45ab
Asger Kromand <crm406@sodas.ku.dk>
2024-12-03 18:54:25 +0100

    Running preprocesses

---
+++ src/filepaths.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# Main path
+MAIN_PATH = Path('/Users/asgerkromand/deep/ANLPDP_exam')
+
+# Data path
+DATA_PATH = MAIN_PATH / 'data'
+
+# Src path
+SRC_PATH = MAIN_PATH / 'src'
+
+# Output path
+OUTPUT_PATH = MAIN_PATH / 'output' 
+
