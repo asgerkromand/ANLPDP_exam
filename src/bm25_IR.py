7849872a41bfe652281c788b58dc82c1723a21bd
Asger Kromand <crm406@sodas.ku.dk>
2024-12-09 00:11:53 +0100

    Changing in rag_test adding bm25

--- src/bm25_IR.py
+++ src/bm25_IR.py
@@ -1,22 +1,20 @@
-# Make a class for BM25 IR model
-# Import necessary libraries
-import re
+# Import
 from nltk.tokenize import word_tokenize
-from tqdm import tqdm
-import nltk
 #nltk.download('stopwords')
 #nltk.download('punkt')
 from nltk.corpus import stopwords
 from rank_bm25 import BM25Okapi
-import json
 import pandas as pd
 import string
 import numpy as np
 
+# Functions
+# Load the csv file
 def load_csv(file_path):
     # Load the csv file
     return pd.read_csv(file_path)
 
+# Preprocess the text
 def preprocess(text):
     # extract and preprocess text8
     stop_words = set(stopwords.words('danish'))
@@ -24,13 +22,18 @@ def preprocess(text):
     tokens = [word for word in tokens if word not in stop_words and word not in string.punctuation]
     return tokens
 
+# Initialize the BM25 corpus
 def init_bm25_corpus(corpus):
     # Initialize the BM25 corpus
     return BM25Okapi(corpus)
 
-def get_ranked_scores(bm25_corpus, query):
+# Get the ranked scores
+def get_ranked_scores(bm25_corpus, query, n_ranked = None):
     # Get scores
     scores = bm25_corpus.get_scores(query)
     # Rank the scores
-    return np.argsort(scores)[::-1]
+    if n_ranked is not None:
+        return np.argsort(scores)[::-1][:n_ranked]
+    else:
+        return np.argsort(scores)[::-1]
 
