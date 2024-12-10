12381beee27b94108ecb4b642b037c7badba05ac
adambobhub <wagnerhoegh.adam@gmail.com>
2024-12-10 14:32:46 +0100

    created generation script

--- neo_generation.py
+++ neo_generation.py
@@ -4,6 +4,8 @@ import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM, T5ForConditionalGeneration
 import ast
+import sys
+
 
 k_retrievals = # find ud af hvordan du kan linke det her til argparse
 
@@ -17,7 +19,6 @@ for col in columns_to_convert:
     dev_set[col] = dev_set[col].apply(ast.literal_eval)
 
 
-
 # load the model and tokenizer
 MODEL_NAME = "KennethTM/gpt-neo-1.3B-danish"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
@@ -27,11 +28,21 @@ model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 DEVICE = "mps" if torch.backends.mps.is_available() else "cpu" # set up for mac here, change to cuda if needed
 model.to(DEVICE)
 
-def tf_idf_gen():
-    neo_answers_tf_idf_k1 = []
+def generate_answers(retriever, k_retrievals, output_path):
+
+    if retriever == 'dense_cls':
+         embeddings_matrix = torch.load('output/embeddings/cls_embeddings_DanskBERT.pt')
+    elif retriever == 'dense_max':
+         embeddings_matrix = torch.load('output/embeddings/max_embeddings_DanskBERT.pt')
+    elif retriever == 'dense_mean':
+         embeddings_matrix = torch.load('output/embeddings/mean_embeddings_DanskBERT.pt')
+    else:
+         pass
+
+    neo_answers = []
 
     # evaluating tf-idf
-    for question, retrieval_column in tqdm(zip(dev_set['question'], dev_set['tf_idf_k1']), desc='Answering questions'):
+    for question, retrieval_column in tqdm(zip(dev_set['question'], dev_set[retriever]), desc='Answering questions'):
 
         # assemble documents into a single string with newlines between each paragraph
         documents = '\n'.join([item for item in retrieval_column][:k_retrievals])
@@ -50,7 +61,6 @@ def tf_idf_gen():
                 input_ids,
                 max_length=max_length,
                 pad_token_id=tokenizer.eos_token_id,
-                no_repeat_ngram_size=7,
                 # generation set to stop at '.' as it otherwise just repeats itself (think it's because we don't sample)
                 eos_token_id=tokenizer.encode(' Spørgsmål')[0]
             )
@@ -59,29 +69,25 @@ def tf_idf_gen():
         answer = tokenizer.decode(outputs[0], skip_special_tokens=True).strip(' Spørgsmål')
 
         # append answer to list
-        neo_answers_tf_idf_k1.append(answer[len(prompt):].strip())  # strip the prompt to leave just the answer
+        neo_answers.append(answer[len(prompt):].strip())  # strip the prompt to leave just the answer
+
+    with open(f'{output_path}/neo_gen_{retriever}_k{k_retrievals}', 'w') as outfile:
+            outfile.write('\n'.join(str(i) for i in neo_answers))
 
 
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser(description="Set the 'KennethTM/gpt-neo-1.3B-danish' model to generate answers")
-    parser.add_argument("output_path", type=str, help="Path to the output file.")
+    parser.add_argument("output_path", type=str, default="output/devset", help="Path to the output file.")
     parser.add_argument("--retriever", type=str, help="Retrieval model (options: 'tf-idf', 'bm25' or 'dense')")
     parser.add_argument("--k_retrievals", type=int, default=1, help="Number of retrievals, ranging from 1 to 3")
     args = parser.parse_args()
 
     output_path = args.output_path
-    embedding_type = args.embedding_type
+    retriever = args.retriever
+    k_retrievals = args.k_retrievals
 
-
-
-    if embedding_type == "CLS":
-        embeddings = create_cls_embedding_matrix(rag_list)
-    elif embedding_type == "max-pool":
-         embeddings = create_max_pool_embedding_matrix(rag_list)
-    elif embedding_type == "mean-pool":
-         embeddings = create_mean_pool_embedding_matrix(rag_list)
-
-    save_tensor(embeddings, output_path)
+    generate_answers(retriever, k_retrievals, output_path)
+    
     print(f"Embeddings saved to {output_path}")
