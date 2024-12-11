a5cbcfb29c4e468095c1603b3c8600fba9cd89b8
Andreas Alkemade <andreas@alkemade.dk>
2024-12-11 12:35:15 +0100

    chg

--- src/performance_eval/eval.py
+++ src/performance_eval/eval.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     
     parser.add_argument('--gold-answers', type=str, default="data/dev_set.csv",
                         help='Path to CSV file containing gold answers')
-    parser.add_argument('--inference-dir', type=str, default="output/inference",
+    parser.add_argument('--inference-dir', type=str, default="output",
                         help='Directory containing model inference output files')
     parser.add_argument('--plot', type=str,
                         help='Path to save plot image')
