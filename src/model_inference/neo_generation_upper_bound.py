a25b96c785a21e1d8834017d00be589869d2a2e1
adambobhub <wagnerhoegh.adam@gmail.com>
2024-12-13 14:28:31 +0100

    created upper bound scripts, pushed upper bound neo results

---
+++ src/model_inference/neo_generation_upper_bound.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import re
+
+# load the model and tokenizer
+MODEL_NAME = "KennethTM/gpt-neo-1.3B-danish"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+# set the device
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu" # set up for mac here, change to cuda if needed
+model.to(DEVICE)
+
+def generate_answers():
+    """
+    Generates answers to legal questions with the huggingface-model 'KennethTM/gpt-neo-1.3B-danish',
+    aided by retrieved legal paragraphs.
+
+    Saves all generated answers to a .txt file after processing.
+    """
+
+    neo_answers_list = []  # List to collect all answers
+
+    # Iterating through questions and retrieved documents
+    for question, answer_paragraph in tqdm(zip(dev_set['question'], dev_set['text']), desc='Answering questions'):
+
+        # Assemble a prompt from the documents, question, and prompting an answer
+        prompt = f"Relevante paragraffer: {answer_paragraph} Spørgsmål: {question} Indsæt svar her baseret på de relevante paragraffer:"
+
+        # Tokenize
+        input_ids = tokenizer(prompt, return_tensors="pt", max_length=924, truncation=True).input_ids.to(DEVICE)
+        max_length = len(input_ids[0]) + 100
+
+        # Generate an answer within torch.no_grad() to save compute
+        with torch.no_grad():
+            outputs = model.generate(
+                input_ids,
+                max_length=max_length,
+                pad_token_id=tokenizer.eos_token_id,
+                eos_token_id=tokenizer.encode(' Spørgsmål')[0]
+            )
+
+        # Decode generated answer
+        answer = tokenizer.decode(outputs[0], skip_special_tokens=True).strip(' Spørgsmål')
+
+        # Strip the prompt to leave just the answer
+        final_answer = answer[len(prompt):].strip()
+
+        final_answer = re.sub('\n', ' ', final_answer)
+        # Append the question and answer as a dictionary to the list
+        neo_answers_list.append(final_answer)
+
+    return neo_answers_list
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser(description="Set the 'KennethTM/gpt-neo-1.3B-danish' model to generate answers")
+    args = parser.parse_args()
+
+    # load dev_set
+    file_path = "../../output/devset/devset_with_contexts.parquet.gzip"
+    with open(file_path, "rb") as f:
+        dev_set = pd.read_parquet(f)
+
+    answers = generate_answers()
+
+    with open(f'../../output/inference/neo_gen_upper_bound.txt', 'w') as file:
+        for answer in answers:
+            file.write(answer + '\n')
+
+    print(f"Answers saved to output/inference/neo_gen_upper_bound.txt")
---
+++ src/model_inference/t5_generation_upper_bound.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+import re
+
+# load model and tokenizer and set device
+model_name = "strombergnlp/dant5-large"  
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu" # set up for mac here, change to cuda if needed
+model.to(DEVICE)
+
+def generate_answers():
+    """
+    Generates answers to legal questions with the huggingface-model 'strombergnlp/dant5-large',
+    aided by retrieved legal paragraphs.
+
+    Returns a list of abovementioned answers.
+    """
+
+    t5_answers_list = []
+
+    # Example question and context
+
+    for question, answer_paragraph in tqdm(zip(dev_set['question'], dev_set['text']), desc='Answering questions'):
+        # Assemble documents into a single string with newlines between each paragraph
+
+        # Format the input for T5
+        input_text = f"Relevante paragraffer: {answer_paragraph}\nSpørgsmål: {question}\nIndsæt svar her baseret på de relevante paragraffer:"
+
+        # Tokenize the input and generate an answer
+        input_ids = tokenizer(input_text, return_tensors="pt", max_length=412, truncation=True).input_ids.to(DEVICE)
+
+        max_length = len(input_ids[0]) + 100
+
+        with torch.no_grad():
+            outputs = model.generate(
+                input_ids,
+                max_length=max_length,
+                pad_token_id=tokenizer.eos_token_id,
+                # generation set to stop at ' Spørgsmål' as it otherwise just repeats itself (think it's because we don't sample)
+                eos_token_id=tokenizer.encode(' Spørgsmål')[0]
+            )
+
+        # Decode and print the generated answer
+        answer = tokenizer.decode(outputs[0], skip_special_tokens=True).strip(' Spørgsmål')
+        answer = re.sub('\n', ' ', answer)
+        t5_answers_list.append(answer)
+
+    return t5_answers_list
+
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser(description="Set the 'strombergnlp/dant5-large")
+    args = parser.parse_args()
+
+    # load dev_set
+    file_path = "../../output/devset/devset_with_contexts.parquet.gzip"
+    with open(file_path, "rb") as f:
+        dev_set = pd.read_parquet(f)
+
+    answers = generate_answers()
+
+    with open(f'../../output/inference/t5_gen_upper_bound.txt', 'w') as file:
+        for answer in answers:
+            file.write(answer + '\n')
+
+    print(f"Answers saved to output/inference/t5_gen_upper_bound.txt")
